530b7ae9b45a072ca786f15207a8ffa8baf6d54a
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-04-03 13:24:46 -0300

    Add Login call using Udacity

--- OnTheMap/ViewController/ViewController.swift
+++ OnTheMap/ViewController/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onLoginBtnClicked(_ sender: Any) {
+        UdacityApiCall.login(email: self.emailField.text ?? "", password: self.passwordField.text ?? "", completion: handleLoginResponse(success:error:))
+    }
+    
+    func handleLoginResponse(success: Bool, error: Error?) {
+        if success {
+            DispatchQueue.main.async {
+                print("SUCCESS")
+                
+            }
+        } else {
+            print("ERROR")
+
+        }
+    }
 }
 
